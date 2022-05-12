f4b251079851d0179515b7668a90afd56d999012
Kyle Lin <minecraft.kyle.train@gmail.com>
2022-05-12 15:47:35 +0800

    Complete NbtCompound canonicalization

--- src/main/java/chaos/unity/loliasm/mixin/NbtCompoundMixin.java
+++ src/main/java/chaos/unity/loliasm/mixin/NbtCompoundMixin.java
@@ -23,8 +23,53 @@ public abstract class NbtCompoundMixin {
         return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
     }
 
-    @ModifyVariable(method = "putByteArray*", at = @At(value = "LOAD"), argsOnly = true, index = 1, print = true)
+    @ModifyVariable(method = "putByteArray*", at = @At(value = "LOAD"), argsOnly = true, index = 1)
     private String modifyPutByteArrayStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putDouble", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutDoubleStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putFloat", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutFloatStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putInt", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutIntStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putIntArray*", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutIntArrayStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putLong", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutLongStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putLongArray*", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutLongArrayStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putShort", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutShortStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putString", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutStringStringKey(String key) {
+        return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
+    }
+
+    @ModifyVariable(method = "putUuid", at = @At("LOAD"), argsOnly = true, index = 1)
+    private String modifyPutUuidStringKey(String key) {
         return LoliStringPool.canonicalize(key, LoliStringPool.NBT_COMPOUND_KEY_ID, false);
     }
 }
